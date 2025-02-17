165eb1eeeab32da580b764ae1b13a80f3f6882f7
Hulk010 <heetbhalani99@gmail.com>
2025-02-17 14:50:05 +0100

    removed unnecessary code

--- train.py
+++ train.py
@@ -115,42 +115,6 @@ def train_model(model, trainloader, valloader, num_epochs, save_dir=r'C:\Users\b
     print(f"Validation F1 Score plot saved at: {f1_plot_path}")
 
     return best_model_path
-        
-
-    
-def preprocessing():
-    transform = {
-        "train": transforms.Compose([
-            transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
-            transforms.Resize((64, 64)),  # Resize to 64x64 as per model need
-            transforms.RandomCrop(64, padding=4),  # Random crop with padding
-            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # This will change brightness of the image
-            transforms.ToTensor(),  
-            transforms.Normalize(mean=[0.5], std=[0.5])  # This will help to normalize the image
-        ]),
-        
-        "val": transforms.Compose([
-            transforms.Grayscale(num_output_channels=1),
-            transforms.Resize((64, 64)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5], std=[0.5])
-        ]),
-    }
-
-    # Location of the dataset and sub directories like train, test and val directory
-    data_dir = r'C:\Users\bhalani\Animal_classification\Dataset\Dataset'  
-    train_dir = f"{data_dir}/train"
-    val_dir = f"{data_dir}/val"
-
-    # This will Load datasets using ImageFolder
-    train_dataset = datasets.ImageFolder(root=train_dir, transform=transform["train"])
-    val_dataset = datasets.ImageFolder(root=val_dir, transform=transform["val"])
-
-    # Define the batch size (As dataset small batch size 8 is taken)
-    batch_size = 8
-
-    trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
-    valloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
     
 if __name__ == "__main__":
     transform = {
